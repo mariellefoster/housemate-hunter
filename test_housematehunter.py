7c1642697608d51bbc82ceecb43aa5ee2dce8b43
Dan Puttick <dputtick@users.noreply.github.com>
2016-10-30 18:15:41 -0400

    Added todos to test file

--- test_housematehunter.py
+++ test_housematehunter.py
@@ -5,4 +5,9 @@ from mac_add import mac_addresses
 def test_mac_clean():
     assert housematehunter.mac_clean("4:3:45:34") == "04:03:45:34"
 
+# things to test:
+# running in normal mode
+# running with -p and -n
+# ifconfig_query should return a valid response
+# determine_class_license should return correct class license
 
